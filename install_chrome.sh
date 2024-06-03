f76ffe5b0eb059b37ee28fe6ee1b2c2557bd75f5
BewChan <21080741.bao@student.iuh.edu.vn>
2024-06-03 19:59:49 +0700

    refile25

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,14 +1,15 @@
 set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
+CHROME_VERSION=114
 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
+  echo "...Downloading Chrome version $CHROME_VERSION"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
+  wget -P ./ https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}-1_amd64.deb
+  dpkg -x ./google-chrome-stable_${CHROME_VERSION}-1_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_${CHROME_VERSION}-1_amd64.deb
   cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
